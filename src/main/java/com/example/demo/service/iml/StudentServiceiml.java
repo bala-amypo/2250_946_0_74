739c00c33a92842846794e6a9dc9882a2227dfad
bala-amypo <bala@amypo.in>
2025-12-17 06:20:39 +0000

    User commit

--- src/main/java/com/example/demo/service/iml/StudentServiceiml.java
+++ src/main/java/com/example/demo/service/iml/StudentServiceiml.java
@@ -1,9 +1,22 @@
 package com.example.demo.service.iml;
 
-@Service
-public class StudentServiceiml extends StudentService{
-    @PostsM
-    public Student createData(@RequestBody Student stu){
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.
+
+@Service
+public class StudentServiceiml implements StudentService{
+    @Autowired
+    StudentRepo sr;
+
+    @Override
+    public Student createData(@RequestBody Student stu){
+           return sr.save(stu);
+    }
+
+    @Override
+    public List<Student> fetchData(){
+        return sr.findAll();
     }
 }
