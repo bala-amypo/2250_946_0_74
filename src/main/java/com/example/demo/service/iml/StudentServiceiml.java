fe7442d82d5e60ba072022167967e621adea1569
bala-amypo <bala@amypo.in>
2025-12-17 06:15:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/iml/StudentServiceiml.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.iml;
+
+@Service
+public class StudentServiceiml extends StudentService{
+    @PostsM
+    public Student createData(@RequestBody Student stu){
+
+    }
+}
